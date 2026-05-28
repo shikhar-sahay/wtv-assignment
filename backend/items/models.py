a460ea6811b24987a9b484e1b3ceb44a1a9d71cc
Noscope999 <sahay.shikhar@gmail.com>
2026-05-28 21:15:52 +0530

    Implemented baseModel abstraction and ItemMode

---
+++ backend/items/models.py
@@ -0,0 +1,35 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Item(BaseModel):
+    key = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+
+    value = models.TextField()
+
+    def __str__(self):
+        return self.key
