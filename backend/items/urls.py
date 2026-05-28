02c11752186312bb1f9742f76a904d02bcb43e9a
Noscope999 <sahay.shikhar@gmail.com>
2026-05-28 21:26:54 +0530

    Implemented serializers and item viewsets

---
+++ backend/items/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import ItemViewSet
+
+
+router = DefaultRouter()
+
+router.register(
+    r"items",
+    ItemViewSet,
+    basename="items",
+)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
